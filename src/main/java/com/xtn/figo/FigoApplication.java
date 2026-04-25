caf91afe1db060c8c615122368d4ad4440680349
achrafbsibiss <achraf.bsibiss11@gmail.com>
2026-04-25 13:25:48 +0100

    The first commit for the Spring project

---
+++ src/main/java/com/xtn/figo/FigoApplication.java
@@ -0,0 +1,13 @@
+package com.xtn.figo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FigoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xtn/figo/FigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xtn.figo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
